ed1d424f0f2f0f9664116e4f60c26e8856e2c39a
ditaisyiyah <ditalara19@gmail.com>
2021-08-26 12:21:51 +0700

    fix h8-footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <div id="app">
     <Navbar v-if="this.$store.state.loginStatus" ></Navbar>
     <router-view/>
-    <!-- <HFooter></HFooter> -->
+    <HFooter></HFooter>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
-// import HFooter from 'vue-hacktiv8-footer'
+import HFooter from 'vue-hacktiv8-footer'
 export default {
   name: 'App',
   components: {
     Navbar,  
-    // HFooter
+    HFooter
   },
   created(){
     const accessToken = localStorage.access_token ;
@@ -29,10 +29,22 @@ export default {
 <style>
 #app {
   font-family: Helvetica, 'Source Sans Pro', 'Merriweather', serif, sans-serif;
-  width: 100vw;
+  width: 1005;
   height: 100vh;
   background: #212121;
   color: #ffffff;
+}
+.footer-h8{
+  width: 100vw;
+  height: 12vh;
+}
+.logo-hacktiv8{
+  height: 6vw;
+}
+.social-link-image{
+  margin-left: 1vw;
+  width: 3vw;
+  height: 6vh;
 }
 
 </style>
